b4b10a52a34a5e8984a48202a43cb4336f0cf03f
Andreas Trøstrup <andreastrstrup@gmail.com>
2020-06-17 13:40:28 +0200

    added get-recyclebin

--- aliases.ps1
+++ aliases.ps1
@@ -1,2 +1,3 @@
 Set-Alias -Name "path" -Value "Get-PathEnvironmentVariable"
 Set-Alias -Name "addpath" -Value "Update-PathEnvironmentVariable"
+Set-Alias -Name "grep" -Value "rg"
---
+++ recbin.ps1
@@ -0,0 +1,4 @@
+function Get-RecycleBin {
+    (New-Object -ComObject Shell.Application).NameSpace(0x0a).Items() |
+	Select-Object Name,Size,Path
+}
