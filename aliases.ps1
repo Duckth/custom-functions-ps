2e7c2c24fb45332be6dcc13cf3e36d7f8ddb4949
Andreas Trøstrup <andreastrstrup@gmail.com>
2020-06-23 13:32:42 +0200

    added aliases

--- aliases.ps1
+++ aliases.ps1
@@ -1,3 +1,5 @@
 Set-Alias -Name "path" -Value "Get-PathEnvironmentVariable"
 Set-Alias -Name "addpath" -Value "Update-PathEnvironmentVariable"
 Set-Alias -Name "grep" -Value "rg"
+Set-Alias -Name "vim" -Value "nvim"
+Set-Alias -Name "vi" -Value "nvim"
