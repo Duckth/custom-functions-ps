7a1ed82bbeece0b319a4af57fc097c061a0e30ac
Andreas Trøstrup <andreastrstrup@gmail.com>
2020-07-22 21:36:08 +0200

    added alias for horrible-downloader

--- aliases.ps1
+++ aliases.ps1
@@ -3,4 +3,6 @@ Set-Alias -Name "addpath" -Value "Update-PathEnvironmentVariable"
 Set-Alias -Name "grep" -Value "rg"
 Set-Alias -Name "vim" -Value "nvim"
 Set-Alias -Name "vi" -Value "nvim"
+Set-Alias -Name "hsd" -Value "horrible-downloader"
+Set-Alias -Name "hsdl" -Value "horrible-downloader"
 
