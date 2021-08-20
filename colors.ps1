e43bef1cec9f04635bae232f0fdded2a60f4a3af
Andreas Trøstrup <8415722+Duckth@users.noreply.github.com>
2021-08-20 19:01:00 +0200

    move aliases to powershell profile

--- colors.ps1
+++ colors.ps1
@@ -1,11 +1,11 @@
 function Get-Colors {
-    [Enum]::GetValues([System.ConsoleColor]) | foreach { Write-Host "$_" -ForegroundColor $_}
+    [Enum]::GetValues([System.ConsoleColor]) | foreach { Write-Host "$_" -ForegroundColor $_ }
 }
 
 function Get-ColorsWithBackground {
     $colors = [enum]::GetValues([System.ConsoleColor])
-    Foreach ($bgcolor in $colors){
-    Foreach ($fgcolor in $colors) { Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine }
-    Write-Host " on $bgcolor"
-}
+    Foreach ($bgcolor in $colors) {
+        Foreach ($fgcolor in $colors) { Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine }
+        Write-Host " on $bgcolor"
+    }
 }
